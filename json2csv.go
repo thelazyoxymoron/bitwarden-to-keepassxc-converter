62cbba29653ab143412ff50409b9aceeed46cac6
Matthew Rabe <matthew.rabe@open-circle.ch>
2023-08-23 11:16:05 +0200

    Remove deprecated package, 'ioutil' is deprecated as of Go 1.16, the same functionality is now provided by package 'io' or package 'os'.

--- json2csv.go
+++ json2csv.go
@@ -4,52 +4,51 @@ import (
 	"encoding/csv"
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
 	"os"
 )
 
 type BitwardenLogin struct {
-    Username string `json:"username"`
-    Password string `json:"password"`
-    TOTP string `json:"totp"`
-    URLs []struct {
-        URL string `json:"uri"`
-    } `json:"uris"`
+	Username string `json:"username"`
+	Password string `json:"password"`
+	TOTP     string `json:"totp"`
+	URLs     []struct {
+		URL string `json:"uri"`
+	} `json:"uris"`
 }
 
 type BitwardenCard struct {
-    CardholderName string `json:"cardholderName"`
-    Brand string `json:"brand"`
-    Number string `json:"number"`
-    ExpiryMonth string `json:"expMonth"`
-    ExpiryYear string `json:"expYear"`
-    Cvv string `json:"code"`
+	CardholderName string `json:"cardholderName"`
+	Brand          string `json:"brand"`
+	Number         string `json:"number"`
+	ExpiryMonth    string `json:"expMonth"`
+	ExpiryYear     string `json:"expYear"`
+	Cvv            string `json:"code"`
 }
 
 type BitwardenItem struct {
-	Name string `json:"name"`
-    Type int `json:"type"`
-    Login BitwardenLogin `json:"login"`
-    Card BitwardenCard `json:"card"`
-    Notes string `json:"notes"`
+	Name  string         `json:"name"`
+	Type  int            `json:"type"`
+	Login BitwardenLogin `json:"login"`
+	Card  BitwardenCard  `json:"card"`
+	Notes string         `json:"notes"`
 }
 
 type BitwardenExport struct {
-    Items []BitwardenItem `json:"items"`
+	Items []BitwardenItem `json:"items"`
 }
 
 func main() {
-    // Check if the correct number of arguments is provided
-    if len(os.Args) != 2 {
-        fmt.Println("Usage: go run main.go <jsonFilePath>")
-        return
-    }
+	// Check if the correct number of arguments is provided
+	if len(os.Args) != 2 {
+		fmt.Println("Usage: go run main.go <jsonFilePath>")
+		return
+	}
 
-    jsonFilePath := os.Args[1]
+	jsonFilePath := os.Args[1]
 	csvFilePath := "filtered_bitwarden.csv"
 
 	// Read the JSON file
-	jsonData, err := ioutil.ReadFile(jsonFilePath)
+	jsonData, err := os.ReadFile(jsonFilePath)
 	if err != nil {
 		fmt.Println("Error reading JSON file:", err)
 		return
@@ -57,11 +56,11 @@ func main() {
 
 	// Unmarshal the JSON data into a slice of BitwardenItem
 	var bwExport BitwardenExport
-    err = json.Unmarshal(jsonData, &bwExport)
-    if err != nil {
-        fmt.Println("Error unmarshaling json: ", err)
-        return
-    }
+	err = json.Unmarshal(jsonData, &bwExport)
+	if err != nil {
+		fmt.Println("Error unmarshaling json: ", err)
+		return
+	}
 
 	// Create a new CSV file
 	csvFile, err := os.Create(csvFilePath)
@@ -85,55 +84,54 @@ func main() {
 
 	// Write each Bitwarden item to the CSV file
 	for _, item := range bwExport.Items {
-        // Export secure notes
-        if (item.Type == 2) {
-            row := []string{"Secure Note", item.Name, "", "", "", item.Notes, ""}
-            err = csvWriter.Write(row)
-            if err != nil {
+		// Export secure notes
+		if item.Type == 2 {
+			row := []string{"Secure Note", item.Name, "", "", "", item.Notes, ""}
+			err = csvWriter.Write(row)
+			if err != nil {
 				fmt.Println("Error writing CSV row:", err)
 				return
-            }
-        } else if (item.Type == 3) {
-            // Export credit/debit cards, store everything in notes column
-            row := []string{
-                "Card",
-                item.Name, 
-                "", 
-                "", 
-                "",
-                "Cardholder Name: " + item.Card.CardholderName + "\n" +
-                "Brand: " + item.Card.Brand + "\n" +
-                "Number: " + item.Card.Number + "\n" +
-                "Expiry: " + item.Card.ExpiryMonth + "-" + item.Card.ExpiryYear + "\n" +
-                "CVV: " + item.Card.Cvv + "\n\n\n" + item.Notes,
-                "",
-            }
-		    err = csvWriter.Write(row)
-		    if err != nil {
-		    	fmt.Println("Error writing CSV row:", err)
-		    	return
-		    }
-        } else {
-		    // In Bitwarden, an item can have multiple URLs, so we'll write each URL as a separate row
-		    for _, url := range item.Login.URLs {
-		    	row := []string{
-                    "Website Login",
-                    item.Name, 
-                    item.Login.Username, 
-                    item.Login.Password, 
-                    url.URL, 
-                    item.Notes,
-                    item.Login.TOTP,
-                }
-		    	err = csvWriter.Write(row)
-		    	if err != nil {
-		    		fmt.Println("Error writing CSV row:", err)
-		    		return
-		    	}
-		    }
-        }
+			}
+		} else if item.Type == 3 {
+			// Export credit/debit cards, store everything in notes column
+			row := []string{
+				"Card",
+				item.Name,
+				"",
+				"",
+				"",
+				"Cardholder Name: " + item.Card.CardholderName + "\n" +
+					"Brand: " + item.Card.Brand + "\n" +
+					"Number: " + item.Card.Number + "\n" +
+					"Expiry: " + item.Card.ExpiryMonth + "-" + item.Card.ExpiryYear + "\n" +
+					"CVV: " + item.Card.Cvv + "\n\n\n" + item.Notes,
+				"",
+			}
+			err = csvWriter.Write(row)
+			if err != nil {
+				fmt.Println("Error writing CSV row:", err)
+				return
+			}
+		} else {
+			// In Bitwarden, an item can have multiple URLs, so we'll write each URL as a separate row
+			for _, url := range item.Login.URLs {
+				row := []string{
+					"Website Login",
+					item.Name,
+					item.Login.Username,
+					item.Login.Password,
+					url.URL,
+					item.Notes,
+					item.Login.TOTP,
+				}
+				err = csvWriter.Write(row)
+				if err != nil {
+					fmt.Println("Error writing CSV row:", err)
+					return
+				}
+			}
+		}
 	}
 
 	fmt.Println("Filtered data has been written to", csvFilePath)
 }
-
